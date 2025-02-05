8ced1533ec0b89d4fca168bcdbc3f5f9bb8e8499
tan8325 <tbach4@student.gsu.edu>
2025-02-05 13:38:55 -0500

    Initial project setup and structure

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Info App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Weather Info App'),
+        ),
+        body: Center(
+          child: Text('Weather Info App'), 
+      ),
+    );
+  }
+}
